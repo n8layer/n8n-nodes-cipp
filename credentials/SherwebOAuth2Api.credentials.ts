2bd92636ff791d481e23c10d0113664e58975fe6
Anthony Birone <anthonyb@elasticit.com>
2025-07-04 14:28:58 -0400

    Remove outdated files and update project structure for new CIPP integration. Deleted CODE_OF_CONDUCT.md, README_TEMPLATE.md, ExampleNode, and HttpBin node files. Added Sherweb node and its corresponding OAuth2 credentials. Updated LICENSE to GNU AGPL v3.0 and modified package.json for new project details. Introduced auto-release workflow and VSCode tasks for improved development experience.

---
+++ credentials/SherwebOAuth2Api.credentials.ts
@@ -0,0 +1,49 @@
+import { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class SherwebOAuth2Api implements ICredentialType {
+	name = 'sherwebOAuth2Api';
+	extends = ['oAuth2Api'];
+	displayName = 'Sherweb OAuth2 API';
+	documentationUrl = 'https://developers.sherweb.com/apis';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Grant Type',
+			name: 'grantType',
+			type: 'hidden',
+			default: 'clientCredentials',
+		},
+		{
+			displayName: 'Access Token URL',
+			name: 'accessTokenUrl',
+			type: 'hidden',
+			default: 'https://api.sherweb.com/auth/oidc/connect/token',
+		},
+		{
+			displayName: 'Client ID',
+			name: 'clientId',
+			type: 'string',
+			required: true,
+			default: '',
+		},
+		{
+			displayName: 'Client Secret',
+			name: 'clientSecret',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			required: true,
+			default: '',
+		},
+		{
+			displayName: 'Subscription Key',
+			name: 'subscriptionKey',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			required: true,
+			default: '',
+		},
+	];
+}
