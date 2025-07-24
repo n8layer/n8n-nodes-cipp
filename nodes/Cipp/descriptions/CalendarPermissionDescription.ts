5c80a747e00a2851d9cac9f671304fa326a92faa
RobertBirone <robertb@elasticit.com>
2025-07-24 17:05:27 -0400

    Add CalendarPermissionDescription: introduce operations and fields for managing calendar permissions, including listing and editing permissions with relevant parameters.

---
+++ nodes/Cipp/descriptions/CalendarPermissionDescription.ts
@@ -0,0 +1,195 @@
+import type { INodeProperties } from 'n8n-workflow';
+
+export const calendarPermissionOperations: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		displayOptions: {
+			show: {
+				resource: [
+					'calendarPermission',
+				],
+			},
+		},
+		options: [
+			{
+				name: 'List Calendar Permissions',
+				value: 'getMany',
+				action: 'List calendar permissions',
+				routing: {
+					request: {
+						method: 'GET',
+						url: '/listcalendarpermissions',
+						qs: {
+							userId: '={{ $parameter.userId }}',
+							tenantFilter: '={{ $parameter.tenantFilter }}',
+						},
+					},
+				},
+			},
+			{
+				name: 'Edit Calendar Permissions',
+				value: 'update',
+				action: 'Edit calendar permissions',
+				routing: {
+					request: {
+						method: 'PATCH',   // TODO: this may be a POST; may need to use customer tenant id
+						url: '/editcalendarpermissions',
+						body: {
+							TenantFilter: '={{ $parameter.tenantFilter }}',
+							permissions: '={{ $parameter.permissions || undefined }}',
+							userid: '={{ $parameter.userId }}',
+							removeaccess: '={{ $parameter.removeaccess || undefined }}',
+							usertogetpermissions: '={{ $parameter.usertogetpermissions || undefined }}',
+							FolderName: '={{ $parameter.FolderName || undefined }}',
+						},
+					},
+				},
+			},
+		],
+		default: 'getMany',
+	},
+];
+export const calendarPermissionFields: INodeProperties[] = [
+	{
+		displayName: 'User ID',
+		name: 'userId',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: ['calendarPermission', 'update'],
+				operation: ['getMany'],
+			},
+		},
+	},
+	{
+		displayName: 'Tenant Filter',
+		name: 'tenantFilter',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: ['calendarPermission'],
+				operation: ['getMany'],
+			},
+		},
+	},
+	{
+		displayName: 'Tenant Filter',
+		name: 'tenantFilter',
+		type: 'string',
+		required: true,
+		default: '',
+		displayOptions: {
+			show: {
+				resource: ['calendarPermission'],
+				operation: ['update'],
+			},
+		},
+	},
+	{
+		displayName: 'Permissions',
+		name: 'permissions',
+		type: 'options',
+		required: true,
+		default: 'Author',
+		displayOptions: {
+			show: {
+				resource: ['calendarPermission'],
+				operation: ['update'],
+			},
+		},
+		options: [
+			{
+				name: 'Author',
+				value: 'Author',
+			},
+			{
+				name: 'AvailabilityOnly',
+				value: 'AvailabilityOnly',
+			},
+			{
+				name: 'Contributor',
+				value: 'Contributor',
+			},
+			{
+				name: 'Editor',
+				value: 'Editor',
+			},
+			{
+				name: 'LimitedDetails',
+				value: 'LimitedDetails',
+			},
+			{
+				name: 'Non Editing Author',
+				value: 'Non Editing Author',
+			},
+			{
+				name: 'Owner',
+				value: 'Owner',
+			},
+			{
+				name: 'Publishing Author',
+				value: 'Publishing Author',
+			},
+			{
+				name: 'Reviewer',
+				value: 'Reviewer',
+			},
+		],
+	},
+	{
+		displayName: 'Remove Access',
+		name: 'removeaccess',
+		type: 'boolean',
+		default: false,
+		displayOptions: {
+			show: {
+				resource: ['calendarPermission'],
+				operation: ['update'],
+			},
+		},
+	},
+	{
+		displayName: 'User to Get Permissions',
+		name: 'usertogetpermissions',
+		type: 'string',
+		default: '',
+		displayOptions: {
+			show: {
+				resource: ['calendarPermission'],
+				operation: ['update'],
+			},
+		},
+	},
+	{
+		displayName: 'Folder Name',
+		name: 'FolderName',
+		type: 'string',
+		default: '',
+		displayOptions: {
+			show: {
+				resource: ['calendarPermission'],
+				operation: ['update'],
+			},
+		},
+	},
+	// {
+	// 	displayName: 'Customer Tenant ID',
+	// 	name: 'customerTenantId',
+	// 	description: 'The tenant ID of the customer to get the calendar permissions for. (Example: contoso.onmicrosoft.com).',
+	// 	type: 'string',
+	// 	default: '',
+	// 	displayOptions: {
+	// 		show: {
+	// 			resource: ['calendarPermission'],
+	// 			operation: ['update'],
+	// 		},
+	// 	},
+	// },
+];
